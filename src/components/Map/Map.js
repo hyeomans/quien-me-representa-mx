0212db26666f9de17990d9cb1f3f82ae466d1fbe
Hector Yeomans <yeomans.leonel@gmail.com>
2021-04-21 14:40:02 -0700

    First iteration of frontend

---
+++ src/components/Map/Map.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+import L from 'leaflet'
+import * as ReactLeaflet from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const { MapContainer } = ReactLeaflet
+
+const Map = ({ children, className, ...rest }) => {
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    })
+  }, [])
+
+  return (
+    <MapContainer className={className} {...rest}>
+      {children(ReactLeaflet)}
+    </MapContainer>
+  )
+}
+
+export default Map
