6f40cf536691ce19acbb879cc16b2b90369091ec
Hector Yeomans <yeomans.leonel@gmail.com>
2021-04-29 08:14:22 -0700

    Adding analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import 'tailwindcss/tailwind.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -7,6 +10,18 @@ const client = new ApolloClient({
 })
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
