cd1c2b59e17916c7a7c6536e3be66173dd684232
Hector Yeomans <yeomans.leonel@gmail.com>
2021-04-29 08:53:29 -0700

    Remove console.log

--- src/components/PuntoMapaDescriptionList.js
+++ src/components/PuntoMapaDescriptionList.js
@@ -1,5 +1,4 @@
 const PuntoEnElMapa = ({ info, representantes }) => {
-  console.log(representantes)
   return (
     <div className="bg-indigo-50 shadow overflow-hidden sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
