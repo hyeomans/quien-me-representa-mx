2663c703f35c2d65bb4c636a2b0afa52e4d91959
Hector Yeomans <yeomans.leonel@gmail.com>
2021-04-29 08:46:44 -0700

    Using trackId directly

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,9 +1,7 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
   if (process.env.NODE_ENV === 'production') {
-    window.gtag('config', GA_TRACKING_ID, {
+    window.gtag('config', process.env.NEXT_PUBLIC_GA_ID, {
       page_path: url,
     })
   }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,31 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import { GA_TRACKING_ID } from '../lib/gtag'
-
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {process.env.NODE_ENV === 'production' ? (
-            <>
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', '${GA_TRACKING_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
               page_path: window.location.pathname,
             });
           `,
-                }}
-              />
-            </>
-          ) : null}
+            }}
+          />
         </Head>
         <body>
           <Main />
