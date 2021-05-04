e0f2332bf9ef4ab8ceb9836a96c500765c52fa1c
Hector Yeomans <yeomans.leonel@gmail.com>
2021-05-04 11:56:34 -0700

    Head meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import PuntoEnElMapa from '../components/PuntoEnElMapa'
 import { useQuery, gql } from '@apollo/client'
+import Head from 'next/head'
 
 const QUERY = gql`
   query locacion($latitud: String!, $longitud: String!) {
@@ -167,44 +168,61 @@ export default function Home() {
   const [position, setPosition] = useState(null)
 
   return (
-    <div className="h-screen w-screen bg-gray-100 flex flex-col justify-between">
-      <Nav />
-      <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:py-4 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="font-extrabold tracking-tight text-gray-900 lg:text-4xl">
-          <span className="block sm:hidden">¿Quiénes me representan en México?</span>
-          <span className="hidden sm:block">
-            ¿Quiénes son mis representantes políticos en México?
-          </span>
-          <span className="block text-indigo-600">Busca tu ubicación y da click en el mapa.</span>
-        </h2>
-      </div>
-      <div className="relative w-full h-full">
-        <Map
-          className="absolute inset-0 z-0"
-          center={[23.681406310669356, -103.31439396326121]}
-          zoom={4}
-          scrollWheelZoom={true}>
-          {({ TileLayer, Marker, useMapEvents }) => (
-            <>
-              <TileLayer
-                url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <LocationMarker
-                Marker={Marker}
-                useMapEvents={useMapEvents}
-                position={position}
-                setPosition={setPosition}
-                setData={setData}
-              />
-            </>
-          )}
-        </Map>
+    <>
+      <Head>
+        <title>¿Quién me representa?</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="¿Quién me representa?" key="title" />
+        <meta charSet="UTF-8" />
 
-        <Informacion data={data} setData={setData} />
-      </div>
+        <meta
+          name="description"
+          content="¿Quiénes son mis representantes políticos? ¿Quién es mi diputado? ¿Quién es mi senador?"></meta>
+        <meta
+          property="og:description"
+          content="¿Quiénes son mis representantes políticos? ¿Quién es mi diputado? ¿Quién es mi senador?"
+          key="ogdesc"
+        />
+      </Head>
+      <div className="h-screen w-screen bg-gray-100 flex flex-col justify-between">
+        <Nav />
+        <div className="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:py-4 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="font-extrabold tracking-tight text-gray-900 lg:text-4xl">
+            <span className="block sm:hidden">¿Quiénes me representan en México?</span>
+            <span className="hidden sm:block">
+              ¿Quiénes son mis representantes políticos en México?
+            </span>
+            <span className="block text-indigo-600">Busca tu ubicación y da click en el mapa.</span>
+          </h2>
+        </div>
+        <div className="relative w-full h-full">
+          <Map
+            className="absolute inset-0 z-0"
+            center={[23.681406310669356, -103.31439396326121]}
+            zoom={4}
+            scrollWheelZoom={true}>
+            {({ TileLayer, Marker, useMapEvents }) => (
+              <>
+                <TileLayer
+                  url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'}
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <LocationMarker
+                  Marker={Marker}
+                  useMapEvents={useMapEvents}
+                  position={position}
+                  setPosition={setPosition}
+                  setData={setData}
+                />
+              </>
+            )}
+          </Map>
 
-      <Footer />
-    </div>
+          <Informacion data={data} setData={setData} />
+        </div>
+
+        <Footer />
+      </div>
+    </>
   )
 }
